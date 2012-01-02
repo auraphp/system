29c48a556ba344ed09bd614042c943aebab0ce0b
Paul M. Jones <pmjones88@gmail.com>
2012-01-02 15:10:07 -0600

    don't assume a namespace

--- web/index.php
+++ web/index.php
@@ -6,7 +6,6 @@
  * @license http://opensource.org/licenses/bsd-license.php BSD
  * 
  */
-namespace Aura\Framework;
 require dirname(__DIR__) . '/package/Aura.Framework/src/Aura/Framework/Bootstrap.php';
-$bootstrap = new Bootstrap();
+$bootstrap = new \Aura\Framework\Bootstrap;
 $bootstrap->execWeb();
