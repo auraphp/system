fd5870d97dc35af510f690c6374c4b600a1a75ea
Paul M. Jones <pmjones88@gmail.com>
2012-11-20 17:59:41 -0600

    use new bootstrap factory

--- web/index.php
+++ web/index.php
@@ -6,6 +6,7 @@
  * @license http://opensource.org/licenses/bsd-license.php BSD
  * 
  */
-require dirname(__DIR__) . '/package/Aura.Framework/src/Aura/Framework/Bootstrap.php';
-$bootstrap = new \Aura\Framework\Bootstrap;
-$bootstrap->execWeb();
+require dirname(__DIR__) . '/package/Aura.Framework/src/Aura/Framework/Bootstrap/Factory.php';
+$factory = new \Aura\Framework\Bootstrap\Factory;
+$web = $factory->newInstance('web');
+$web->exec();
